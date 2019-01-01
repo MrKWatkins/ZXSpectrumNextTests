79e1d16417d1805f161fae92f50516f7331fe708
ped7g <ped@7gods.org>
2019-01-02 00:40:41 +0100

    timing banking switch modified to run @14MHz
    old screenshots removed, added new ones

--- Tests/Timing/Changing8kBank/Main.asm
+++ Tests/Timing/Changing8kBank/Main.asm
@@ -6,27 +6,32 @@
 	INCLUDE "..\..\Macros.asm"
 	INCLUDE "..\..\TestFunctions.asm"
 
-Start
-	call StartTest
-
+Start:
+	call   StartTest
 	ei
 
-TestLoop
+	;; set Next speed to turbo mode at 14MHz
+	; read current settings (to keep PS/2 and divMMC paging configuration intact)
+	NEXTREG2A  PERIPHERAL_2_NR_06
+	or     $80         ; switch turbo mode ON
+	NEXTREG_A  PERIPHERAL_2_NR_06
+	; set up 14MHz speed
+	NEXTREG_nn TURBO_CONTROL_NR_07, %10
+
+TestLoop:
 	halt
+	call   StartTiming
 
-	call StartTiming
-
-	ld a, 0				; Repeat switch 256 times.
-
-@BankSwitchWithNextReg
+	ld     bc, 7       ; Repeat switch 7*256 times. (b=0)
+@BankSwitchWithNextReg:
 	NEXTREG_nn MMU6_C000_NR_56, 32
 	NEXTREG_nn MMU6_C000_NR_56, 33
+	djnz   @BankSwitchWithNextReg
+	dec    c
+	jr     nz,@BankSwitchWithNextReg
 
-        dec a
-        jr nz, @BankSwitchWithNextReg
+	call   EndTiming
 
-	call EndTiming
-
-	jr TestLoop
+	jr     TestLoop
 
 	savesna "Chg8kBan.sna", Start
