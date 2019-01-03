da6e388e545bbddc54fa1c2836f388cd0b2a1200
ped7g <ped@7gods.org>
2019-01-03 23:22:45 +0100

    Sprites/Transparency test: fixed to index-transparency
    Also extra check of default index after reset added
    
    (#CSpect2.2.0 interestingly does render sprites as if index $E3 is set,
    but reading the next reg before first set returns 0. After first set the
    reading returns the value which was set last time)

--- Tests/Sprites/Transparency/Main.asm
+++ Tests/Sprites/Transparency/Main.asm
@@ -4,36 +4,50 @@
 
 	INCLUDE "..\..\Constants.asm"
 	INCLUDE "..\..\Macros.asm"
-	INCLUDE "..\..\TestData.asm"
 	INCLUDE "..\..\TestFunctions.asm"
 
 Start
 	call StartTest
 
 	NEXTREG_nn SPRITE_CONTROL_NR_15, %00000001	; Set sprites over Layer2 and ULA and enable sprites.
+	NEXTREG_nn PALETTE_CONTROL_NR_43, $20   ; select sprite palette 0, ULANext off
 
 	call LoadPattern
 
 	call ShowSprite
 
-	call SetFirstColourInSpritePaletteToTransparent
+	call CheckDefaultTransparencyIndex
+	NEXTREG_nn PALETTE_INDEX_NR_40, 1       ; Change palette[S][0][1] = A
+	NEXTREG_A PALETTE_VALUE_NR_41
+
+	call SetIdx2ColourInSpritePaletteToTransparent
 
 	call EndTest
 
-SetFirstColourInSpritePaletteToTransparent
-	NEXTREG_nn PALETTE_CONTROL_NR_43, $20	; We're changing the sprite palette.
-	NEXTREG_nn PALETTE_INDEX_NR_40, 0		; Change the first colour in the palette, index 0.
-	NEXTREG_nn PALETTE_VALUE_9BIT_NR_44, $e3
-	NEXTREG_nn PALETTE_VALUE_9BIT_NR_44, 0	; Set to default transparent colour.
-	ret
+CheckDefaultTransparencyIndex:              ; returns green (OK)/red (ERR) colour in A
+    ; test sprite transparency index
+    NEXTREG2A SPRITE_TRANSPARENCY_I_NR_4B
+    cp  $E3                                 ; A=E3 should be true after reset
+    ld  a,$0C                               ; return green colour when OK
+    ret z
+    ld  a,$E0                               ; return red when error
+    ret
 
-ShowSprite
+SetIdx2ColourInSpritePaletteToTransparent:
+    ; Set sprite transparent-index to 2
+    NEXTREG_nn SPRITE_TRANSPARENCY_I_NR_4B, 2
+    ; Set also colour 2 to red to signalize error in emulation, if it is somehow visible
+    NEXTREG_nn PALETTE_INDEX_NR_40, 2       ; Change palette[S][0][2] = red
+    NEXTREG_nn PALETTE_VALUE_NR_41, $e0
+    ret
+
+ShowSprite:
 	ld bc, SPRITE_ATTRIBUTE_P_57
 	ld a, 64
 	out (c), a
 	out (c), a					; Display the sprite at 64, 64.
-	out (c), 0
-	ld a, %10000000
+	out (c), 0                  ; pal.ofs=0, mirror/rotation=0, X8=0
+	ld a, %10000000             ; visible=1, 4 bytes struct only, pattern=0
 	out (c), a					; Make the sprite visible.
 	ret
 
@@ -41,35 +55,29 @@ LoadPattern
 	ld bc, SPRITE_STATUS_SLOT_SELECT_P_303B
 	out (c), 0					; Write to pattern slot 0.
 
-	ld bc, SPRITE_PATTERN_P_5B
+	ld bc, SPRITE_PATTERN_P_5B  ; b=0 as counter
 	ld hl, Pattern
-	ld a, 0
-
 @Loop
-	outi						; Write a byte to the port, increase HL and decrease B.
-	inc b						; Increase B so it points at the port again.
-	dec a
-	jr nz, @Loop
-
+	otir                        ; send 256 bytes to $xx5B port (uploads whole pattern)
 	ret
 
-; Sprite is divided into four squares, top left bottom right are coloured index 0, the other two coloured index 1.
-Pattern
-	defb $00, $00, $00, $00, $00, $00, $00, $00, $01, $01, $01, $01, $01, $01, $01, $01
-	defb $00, $00, $00, $00, $00, $00, $00, $00, $01, $01, $01, $01, $01, $01, $01, $01
-	defb $00, $00, $00, $00, $00, $00, $00, $00, $01, $01, $01, $01, $01, $01, $01, $01
-	defb $00, $00, $00, $00, $00, $00, $00, $00, $01, $01, $01, $01, $01, $01, $01, $01
-	defb $00, $00, $00, $00, $00, $00, $00, $00, $01, $01, $01, $01, $01, $01, $01, $01
-	defb $00, $00, $00, $00, $00, $00, $00, $00, $01, $01, $01, $01, $01, $01, $01, $01
-	defb $00, $00, $00, $00, $00, $00, $00, $00, $01, $01, $01, $01, $01, $01, $01, $01
-	defb $00, $00, $00, $00, $00, $00, $00, $00, $01, $01, $01, $01, $01, $01, $01, $01
-	defb $01, $01, $01, $01, $01, $01, $01, $01, $00, $00, $00, $00, $00, $00, $00, $00
-	defb $01, $01, $01, $01, $01, $01, $01, $01, $00, $00, $00, $00, $00, $00, $00, $00
-	defb $01, $01, $01, $01, $01, $01, $01, $01, $00, $00, $00, $00, $00, $00, $00, $00
-	defb $01, $01, $01, $01, $01, $01, $01, $01, $00, $00, $00, $00, $00, $00, $00, $00
-	defb $01, $01, $01, $01, $01, $01, $01, $01, $00, $00, $00, $00, $00, $00, $00, $00
-	defb $01, $01, $01, $01, $01, $01, $01, $01, $00, $00, $00, $00, $00, $00, $00, $00
-	defb $01, $01, $01, $01, $01, $01, $01, $01, $00, $00, $00, $00, $00, $00, $00, $00
-	defb $01, $01, $01, $01, $01, $01, $01, $01, $00, $00, $00, $00, $00, $00, $00, $00
+; Sprite is divided into four squares, top left bottom right are coloured index 2, the other two coloured index 1.
+Pattern:            ; avoiding index 0 in case some emulator defaults to 0 with $4B
+	defb $02, $02, $02, $02, $02, $02, $02, $02, $01, $01, $01, $01, $01, $01, $01, $01
+	defb $02, $02, $02, $02, $02, $02, $02, $02, $01, $01, $01, $01, $01, $01, $01, $01
+	defb $02, $02, $02, $02, $02, $02, $02, $02, $01, $01, $01, $01, $01, $01, $01, $01
+	defb $02, $02, $02, $02, $02, $02, $02, $02, $01, $01, $01, $01, $01, $01, $01, $01
+	defb $02, $02, $02, $02, $02, $02, $02, $02, $01, $01, $01, $01, $01, $01, $01, $01
+	defb $02, $02, $02, $02, $02, $02, $02, $02, $01, $01, $01, $01, $01, $01, $01, $01
+	defb $02, $02, $02, $02, $02, $02, $02, $02, $01, $01, $01, $01, $01, $01, $01, $01
+	defb $02, $02, $02, $02, $02, $02, $02, $02, $01, $01, $01, $01, $01, $01, $01, $01
+	defb $01, $01, $01, $01, $01, $01, $01, $01, $02, $02, $02, $02, $02, $02, $02, $02
+	defb $01, $01, $01, $01, $01, $01, $01, $01, $02, $02, $02, $02, $02, $02, $02, $02
+	defb $01, $01, $01, $01, $01, $01, $01, $01, $02, $02, $02, $02, $02, $02, $02, $02
+	defb $01, $01, $01, $01, $01, $01, $01, $01, $02, $02, $02, $02, $02, $02, $02, $02
+	defb $01, $01, $01, $01, $01, $01, $01, $01, $02, $02, $02, $02, $02, $02, $02, $02
+	defb $01, $01, $01, $01, $01, $01, $01, $01, $02, $02, $02, $02, $02, $02, $02, $02
+	defb $01, $01, $01, $01, $01, $01, $01, $01, $02, $02, $02, $02, $02, $02, $02, $02
+	defb $01, $01, $01, $01, $01, $01, $01, $01, $02, $02, $02, $02, $02, $02, $02, $02
 
 	savesna "SpritTra.sna", Start
--- Tests/Sprites/Transparency/ReadMe.txt
+++ Tests/Sprites/Transparency/ReadMe.txt
@@ -1,3 +1,7 @@
-This demo creates a sprite from colour indexes 0 and 1 in the palette. It then sets index 0 to be the transparent colour.
+This demo creates a sprite from colour indexes 1 and 2. It then sets index 2 to be the transparent colour.
 
-We should see two blue squares on a white background - the top left/bottom right squares of the sprite should be transparent and therefore white.
+Top-Left (◰) and Bottom-Right (◲) squares should be transparent = WHITE
+(if RED, sprite transparency failed).
+
+Top-Right and Bottom-Left (▞) squares should be GREEN
+(if RED, the sprite transparency index after reset does not read default $E3 value)
