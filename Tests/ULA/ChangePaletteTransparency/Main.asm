3c35d19e359201577534b153e46aa35c93d20624
ped7g <ped@7gods.org>
2019-01-03 15:58:43 +0100

    ULA/ChangePaletteTransparency test: fixed to set all relevant NextRegs
    Plus screenshots updated to new version of test (ZEsarUX7.1 and
    
    The functional (affecting results) modifications of test:
    - the ULANext colour translation (palette) enabled (bit0 @NextReg$43)
    - INK mas set to "7" (to extract paper value "7" from attribute value
    $38)
    - transparency fallback colour set to $1F (raw cyan)
    
    Plus some code cleanup/refactoring (not affecting result).

--- Tests/ULA/ChangePaletteTransparency/Main.asm
+++ Tests/ULA/ChangePaletteTransparency/Main.asm
@@ -7,24 +7,23 @@
 	INCLUDE "..\..\TestData.asm"
 	INCLUDE "..\..\TestFunctions.asm"
 
-Start
+Start:
 	call StartTest
 
-	NEXTREG_nn SPRITE_CONTROL_NR_15, %00010100	; Set ULA over Layer2 over sprites, with sprites not visible.
+    ; Set ULA over Layer2 over sprites, with sprites not visible.
+    NEXTREG_nn SPRITE_CONTROL_NR_15, %00010100
+    NEXTREG_nn PALETTE_FORMAT_NR_42, 7      ; INK bitmask (0..7 is INK, 0..31 PAPER)
+        ; this INK mask "7" is "compatible" with default attribute $38 filled in VRAM
+    NEXTREG_nn PALETTE_CONTROL_NR_43, 1     ; select first-ULA palette + enable ULANext
+    ; palette[ULA][0][135] change (paper 7 + border 7 colour to $E300)
+    NEXTREG_nn PALETTE_INDEX_NR_40, 135     ; Change paper 7 = 128 + 7 = 135.
+    NEXTREG_nn PALETTE_VALUE_9BIT_NR_44, $E3
+    NEXTREG_nn PALETTE_VALUE_9BIT_NR_44, $00; Set to default "pink" transparent colour.
+    ; bright cyan as transparency fallback
+    NEXTREG_nn TRANSPARENCY_FALLBACK_COL_NR_4A, $1F
 
 	call FillLayer2WithTestData
 
-	call ChangeUlaPalette
-
 	call EndTest
-
-ChangeUlaPalette
-	NEXTREG_nn PALETTE_CONTROL_NR_43, 0		; We're changing the ULA palette.
-	NEXTREG_nn PALETTE_INDEX_NR_40, 135		; Change paper 7 = 128 + 7 = 135.
-	NEXTREG_nn PALETTE_VALUE_9BIT_NR_44, $e3
-	NEXTREG_nn PALETTE_VALUE_9BIT_NR_44, 0	; Set to default transparent colour.
-	ret
-
-
 
 	savesna "CPalTran.sna", Start
