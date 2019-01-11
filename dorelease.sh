3b020b794f2c8a6e9a52ccab7af4760882041a97
ped7g <ped@7gods.org>
2019-01-11 17:15:14 +0100

    flat-release bash script added (source)
    This is experiment, exploring the idea that "flat" release folder
    containing all tests together is better UX for non-developer users than
    keeping snapshot files across many subfolders.
    
    Also the "release" dir will fit 8.3 name scheme (as long as file name of
    snapshot is 8.3), in case some limited file system is involved, where
    long file names of test folders would fail.

---
+++ dorelease.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+# Script to search for new snapshot files in test directories, and refresh the "release"
+# folder with them.
+shopt -s globstar nullglob
+# create release folder, if it does not exist yet
+mkdir -p release
+unset -v updatedSomeFile
+# for all snapshots in Tests
+for f in Tests/**/*.sna; do
+    echo -n Found snapshot: $f
+    dirpath=`dirname $f`
+    snapname=`basename $f`
+    basename=`basename -s .sna $snapname`
+    # *MOVE* snapshot into release folder
+    # - move, so developer of test can tell if he did refresh the release directory
+    #   (but for non-developers the "release" directory should be "everything they need")
+    mv $f release/$snapname
+    # check if "ReadMe.txt" exists and copy it under <snapshot-base-name>.txt into release
+    for readme in $dirpath/{ReadMe,readme,README,Readme}.{txt,TXT} ; do
+        [[ -e $readme ]] && cp $readme release/$basename.txt \
+            && echo -n , $basename.txt updated
+    done
+    # check for most recent board**.png/jpg screenshot and copy it
+    # under <snapshot-base-name>.png/jpg name into release folder
+    unset -v boardpic
+    for bpicsearch in $dirpath/{board,Board}*{.png,.jpg} ; do
+        [[ $bpicsearch -nt $boardpic ]] && boardpic=$bpicsearch
+    done
+    [[ -e $boardpic ]] && cp $boardpic release/$basename.${boardpic##*.} \
+        && echo -n , board pic updated
+    echo ""     # add newline after whole line of info was produced for particular snap
+    updatedSomeFile=1
+done
+# refresh date when release folder was updated
+[[ $updatedSomeFile ]] && date -u +"%F %T %Z" > release/updated
