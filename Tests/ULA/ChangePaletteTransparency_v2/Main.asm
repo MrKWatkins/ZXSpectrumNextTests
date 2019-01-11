a9d0c60bcf45cbbe6ace659e81ed1b81281726f8
ped7g <ped@7gods.org>
2019-01-11 12:00:16 +0100

    ChangePaletteTransparency_v2: modified to rely less on "defaults", new pics
    After some thinking and experience from some people helping to run tests
    on real board, I came to this philophical conclusion:
    
    Testing defaults of machine in ordinary tests seems to be
    counterproductive, as running such tests on real board would require
    power-on state before each test (even when switching between tests).
    
    When "default values" are part of test, they should be reported more
    like "warning" than "error". And the test should be meaningful even with
    wrong defaults, i.e. it will still test some particular feature properly
    and report conclusive result about its functionality (avoiding failures
    just because of wrong defaults).

--- Tests/ULA/ChangePaletteTransparency_v2/Main.asm
+++ Tests/ULA/ChangePaletteTransparency_v2/Main.asm
@@ -12,16 +12,20 @@ Start:
 
     ; Set ULA over Layer2 over sprites, with sprites not visible.
     NEXTREG_nn SPRITE_CONTROL_NR_15, %00010100
-    ; keep INK mask NR$42 at default value (should be 15 = 0..15 INK, 0..15 PAPER)
+    ; reset INK mask NR$42 to default value 15 (0..15 INK, 0..15 PAPER)
+    NEXTREG_nn PALETTE_FORMAT_NR_42, 15     ; INK bitmask (0..15 is INK, 0..15 PAPER)
     ; this INK mask 15 for default attribute $38 = INK 8, PAPER 3
     NEXTREG_nn PALETTE_CONTROL_NR_43, 1     ; select first-ULA palette + enable ULANext
     ; palette[ULA][0][131] change (paper 3 colour to $E300)
     NEXTREG_nn PALETTE_INDEX_NR_40, 128+3   ; Change paper 3
     NEXTREG_nn PALETTE_VALUE_9BIT_NR_44, $E3
     NEXTREG_nn PALETTE_VALUE_9BIT_NR_44, $00; Set to default "pink" transparent colour.
+    ; border is still 7 = offset 128+7
+    NEXTREG_nn PALETTE_INDEX_NR_40, 128+7   ; Change border 7
+    NEXTREG_nn PALETTE_VALUE_9BIT_NR_44, $10
+    NEXTREG_nn PALETTE_VALUE_9BIT_NR_44, $00; Set to "green"
     ; bright cyan as transparency fallback
     NEXTREG_nn TRANSPARENCY_FALLBACK_COL_NR_4A, $1F
-    ; border is still 7 = offset 128+7 = should be by default white? Or is it UB?
 
 	call FillLayer2WithTestData
 
