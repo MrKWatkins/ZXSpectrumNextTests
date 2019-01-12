f527267e2f4953d273ca3ec5f6395afb9332171f
ped7g <ped@7gods.org>
2019-01-12 13:10:39 +0100

    Timing/Changing8kBank: making the init of machine more complete
    * also RAM contention is now set ON (like classic ZX48k)
    * and layer2 is disabled (to make cores 2.00.xx to run at full 14MHz)

--- Tests/Timing/Changing8kBank/Main.asm
+++ Tests/Timing/Changing8kBank/Main.asm
@@ -13,22 +13,41 @@ Start:
 	;; set Next speed to turbo mode at 14MHz
 	; read current settings (to keep PS/2 and divMMC paging configuration intact)
 	NEXTREG2A  PERIPHERAL_2_NR_06
-	or     $80         ; switch turbo mode ON
+	or     $80          ; switch turbo mode ON
 	NEXTREG_A  PERIPHERAL_2_NR_06
 	; set up 14MHz speed
 	NEXTREG_nn TURBO_CONTROL_NR_07, %10
+	; set RAM contention ON
+	NEXTREG2A  PERIPHERAL_3_NR_08
+	and    ~$40         ; enable RAM contention
+	NEXTREG_A  PERIPHERAL_3_NR_08
+
+    ld  bc, LAYER2_ACCESS_P_123B
+    out (c), 0          ; make sure Layer2 is DISABLED
 
 TestLoop:
 	halt
 	call   StartTiming
 
-	ld     bc, 7       ; Repeat switch 7*256 times. (b=0)
-@BankSwitchWithNextReg:
-	NEXTREG_nn MMU6_C000_NR_56, 32
-	NEXTREG_nn MMU6_C000_NR_56, 33
+	ld     b, 0         ; Repeat switch block 256 times.
+@BankSwitchWithNextReg: ; switch 16 pages in
+	NEXTREG_nn MMU6_C000_NR_56, 11
+	NEXTREG_nn MMU6_C000_NR_56, 12
+	NEXTREG_nn MMU6_C000_NR_56, 13
+	NEXTREG_nn MMU6_C000_NR_56, 14
+	NEXTREG_nn MMU6_C000_NR_56, 15
+	NEXTREG_nn MMU6_C000_NR_56, 16
+	NEXTREG_nn MMU6_C000_NR_56, 17
+	NEXTREG_nn MMU6_C000_NR_56, 18
+	NEXTREG_nn MMU6_C000_NR_56, 19
+	NEXTREG_nn MMU6_C000_NR_56, 20
+	NEXTREG_nn MMU6_C000_NR_56, 21
+	NEXTREG_nn MMU6_C000_NR_56, 22
+	NEXTREG_nn MMU6_C000_NR_56, 23
+	NEXTREG_nn MMU6_C000_NR_56, 24
+	NEXTREG_nn MMU6_C000_NR_56, 25
+	NEXTREG_nn MMU6_C000_NR_56, 26
 	djnz   @BankSwitchWithNextReg
-	dec    c
-	jr     nz,@BankSwitchWithNextReg
 
 	call   EndTiming
 
--- Tests/Timing/Changing8kBank/ReadMe.txt
+++ Tests/Timing/Changing8kBank/ReadMe.txt
@@ -1,1 +1,4 @@
 This times how long it takes to swap in and out an 8k bank.
+
+The test code is placed at $6000, memory contention is switched ON and Turbo mode is
+set to 14MHz, Layer2 is DISABLED (i.e. real board is expected to run at full 14MHz).
