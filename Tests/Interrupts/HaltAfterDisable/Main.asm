b40d5d60dd0ef0553147ae6bcdc11a196a5f4b0d
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-02-05 01:58:36 +0100

    Interrupts/HalfAfterDisable: modified look + added detection of INT-handler
    The test does now not only check if halt holds, but the result also
    differs depending whether interrupt handler was fired (attributes
    damaged) or just halt was skipped without interrupt (red border only).
    
    Also "OK" state is now modified to "green" border.

--- Tests/Interrupts/HaltAfterDisable/Main.asm
+++ Tests/Interrupts/HaltAfterDisable/Main.asm
@@ -7,14 +7,21 @@
 
 Start
 	call StartTest
+    ld a, GREEN
+    out (ULA_P_FE), a	; Set the border to green
 
 	di
+
+	; also move stack pointer into attributes, so if interrupt handler is executed
+	; it will be visible on damaged attributes
+	ld sp,MEM_ZX_ATTRIB_5800+16*32
 
 	halt		; Test should not proceed past this point!
 
     ld a, RED
     out (ULA_P_FE), a	; Set the border to red.
 
-	call EndTest
+freezeHere:     ; freeze without touching stack to make attributes damaged only by handler
+    jr  freezeHere
 
 	savesna "DIHalt.sna", Start
--- Tests/Interrupts/HaltAfterDisable/ReadMe.txt
+++ Tests/Interrupts/HaltAfterDisable/ReadMe.txt
@@ -1,3 +1,7 @@
-This test disables interrupts, calls halt, and then changes the border colour to red.
+The test disables interrupts and does "halt", the machine should freeze there with
+GREEN BORDER and full WHITE PAPER = OK.
 
-The border colour should stay white as the halt instruction should wait indefinitely.
+If the halt is passed and further instructions are executed, the BORDER will turn RED.
+
+If the interrupt handler will get executed, the attributes at end of 2/3 of screen
+will get damaged, as the stack pointer points there.
