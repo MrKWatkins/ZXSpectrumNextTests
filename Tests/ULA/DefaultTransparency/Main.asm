364fb26cea59a6569719a7697d5db8db684fc21f
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-02-24 13:20:45 +0100

    ULA/DefaultTransparency - reset NR_14, added informal text to result screen

--- Tests/ULA/DefaultTransparency/Main.asm
+++ Tests/ULA/DefaultTransparency/Main.asm
@@ -6,11 +6,18 @@
 	INCLUDE "../../Macros.asm"
 	INCLUDE "../../TestData.asm"
 	INCLUDE "../../TestFunctions.asm"
+    INCLUDE "../../OutputFunctions.asm"
 
 Start
 	call StartTest
+    ld   hl,TestTxt
+    ld   de,MEM_ZX_SCREEN_4000
+    call OutStringAtDe
 
 	NEXTREG_nn SPRITE_CONTROL_NR_15, %00010100	; Set ULA over Layer2 over sprites, with sprites not visible.
+    ; default "pink" as transparency colour and bright cyan as transparency fallback
+    NEXTREG_nn GLOBAL_TRANSPARENCY_NR_14, $E3
+    NEXTREG_nn TRANSPARENCY_FALLBACK_COL_NR_4A, $1F
 
 	call FillLayer2WithTestData
 
@@ -18,5 +25,8 @@ Start
 	FILL_AREA MEM_ZX_ATTRIB_5800, 32*24, P_MAGENTA|A_BRIGHT
 
 	call EndTest
+
+TestTxt:
+    db  ' Magenta paper + this text = OK', 0
 
 	savesna "DefTrans.sna", Start
