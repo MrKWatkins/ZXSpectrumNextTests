3dcdd074f1015164614fbbff83131c4244183399
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-05-17 12:21:48 +0200

    auto.bas loader: removing extra REM ahead of SAVE
    Still needs testing + vetting by somebody on actual board.

--- Tools/dorelease.sh
+++ Tools/dorelease.sh
@@ -36,7 +36,9 @@ done
 # copy the readme every time
 cp release-README.txt "release/!!README.txt"
 # compile/copy the basic loader every time, if available
-command -v bas2tap 1>/dev/null && bas2tap -q -a1 -s"auto.bas" Tools/auto.bas.txt release/auto.tap
+command -v bas2tap 1>/dev/null \
+    && echo -e "\e[95mBuilding\e[0m BASIC autoloader \e[96mauto.bas\e[0m\e[0m" \
+    && bas2tap -q -a1 -s"auto.bas" Tools/auto.bas.txt release/auto.tap
 [[ -s Tools/auto.bas ]] && cp Tools/auto.bas release/auto.bas
 # check if some file got leftovers (there should be "3" files of same base name at most)
 source Tools/chkReleaseAbundance.sh
