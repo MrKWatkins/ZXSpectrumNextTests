6f00a73cf42e1f06b9f0199461d9f5acaaf25789
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-12-04 10:22:06 +0100

    Timing/Changing8kBank (both): changed to 3.5Mhz mode, turbo disables contention

--- Tests/Timing/Changing8kBank_NoContention/Main.asm
+++ Tests/Timing/Changing8kBank_NoContention/Main.asm
@@ -10,31 +10,45 @@
 Start:
 	call   StartTest
 	ld     hl,TestTxt
-	ld     de,MEM_ZX_SCREEN_4000
+	ld     de,MEM_ZX_SCREEN_4000+1
 	call   OutStringAtDe
+	ld     d,$E0
+	ld     bc,$0118
+	ld     hl,MEM_ZX_SCREEN_4000
+	call   FillSomeUlaLines
+	ld     d,$80
+	ld     bc,$0118
+	ld     hl,MEM_ZX_SCREEN_4000+$400
+	call   FillSomeUlaLines
+	ld     a,$FC
+	ld     (MEM_ZX_SCREEN_4000),a
+	ld     (MEM_ZX_SCREEN_4000+$800),a
+	ld     (MEM_ZX_SCREEN_4000+$1000),a
+	ld     a,A_BRIGHT|P_GREEN|BLACK
+	ld     hl,MEM_ZX_ATTRIB_5800
+	ld     de,32
+	ld     b,24
+.doAttribsRainbow:
+	ld     (hl),a
+	add    hl,de
+	add    a,P_BLUE
+	and    P_WHITE
+	or     A_BRIGHT|P_GREEN
+	djnz   .doAttribsRainbow
 	ei
 
-	;; set Next speed to turbo mode at 14MHz
-	; read current settings (to keep PS/2 and divMMC paging configuration intact)
-	NEXTREG2A  PERIPHERAL_2_NR_06
-	or     $80          ; switch turbo mode ON
-	NEXTREG_A  PERIPHERAL_2_NR_06
-	; set up 14MHz speed
-	NEXTREG_nn TURBO_CONTROL_NR_07, %10
+	; set up base 3.5MHz speed (turbo modes have contention OFF)
+	NEXTREG_nn TURBO_CONTROL_NR_07, %00
 	; set RAM contention OFF
 	NEXTREG2A  PERIPHERAL_3_NR_08
 	or     $40          ; disable RAM contention
 	NEXTREG_A  PERIPHERAL_3_NR_08
-    ; make sure Layer2 is DISABLED
-    ld      bc, LAYER2_ACCESS_P_123B
-    xor     a
-    out     (c), a
 
 TestLoop:
 	halt
 	call   StartTiming
 
-	ld     b, 0         ; Repeat switch block 256 times.
+	ld     b,128           ; Repeat switch block N times.
 @BankSwitchWithNextReg: ; switch 16 pages in
     ; this is intentionally as multiple NEXTREG instructions not in loop, to make major
     ; part of time spent come from the switching itself, and not from loop instructions
